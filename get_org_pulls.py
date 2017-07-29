67709aec80625b124987b64bdc813132c7365ea8
Jacqueline Outka <jacqueline@outka.xyz>
2017-07-29 19:08:45 -0400

    Reorganize structure

--- get_org_pulls.py
+++ get_org_pulls.py
@@ -1,14 +1,16 @@
 import requests
 import json
-# for each repository, list all pull requests
 
-organization_data = requests.get('https://api.github.com/orgs/lodash/repos').json()
-repo_names = []
-for index, item in enumerate(organization_data):
-    repo_names.append(organization_data[index][u'name'])
-print(repo_names)
-pull_requests = []
-for repo in repo_names:
-    request = requests.get('https://api.github.com/repos/lodash/{}/pulls'.format(repo))
-    pull_requests.append(request.json())
-print(pull_requests)
+def get_repo_names(url):
+    organization_data = requests.get(url).json()
+    return [organization_data[index][u'name'] for (index, item) in enumerate(organization_data)]
+
+def get_pull_requests(repo_names):
+    return [requests.get('https://api.github.com/repos/lodash/{}/pulls'.format(repo)).json() for repo in repo_names]
+
+if __name__ == '__main__':
+    repo_names = get_repo_names('https://api.github.com/orgs/lodash/repos')
+    pull_requests = get_pull_requests(repo_names)
+    for item in pull_requests:
+        print(item)
+        print("\n")
