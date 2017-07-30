c6f1da5ea0f29c31099181b6b56bce9ff18bdc54
Jacqueline Outka <jacqueline@outka.xyz>
2017-07-30 15:13:41 -0400

    Add param to include closed as well as open PRs in result

--- get_org_pulls.py
+++ get_org_pulls.py
@@ -7,11 +7,11 @@ def get_repo_names(url):
     return [organization_data[index][u'name'] for (index, item) in enumerate(organization_data)]
 
 def get_pull_requests(repo_names):
-    return [requests.get('https://api.github.com/repos/lodash/{}/pulls'.format(repo), auth=(os.environ['GITHUB_TOKEN'], 'x-oauth-basic')).json() for repo in repo_names]
+    params = {'state': 'all'}
+    return [requests.get('https://api.github.com/repos/lodash/{}/pulls'.format(repo), params, auth=(os.environ['GITHUB_TOKEN'], 'x-oauth-basic')).json() for repo in repo_names]
 
 if __name__ == '__main__':
     repo_names = get_repo_names('https://api.github.com/orgs/lodash/repos')
     pull_requests = get_pull_requests(repo_names)
     for item in pull_requests:
         print(json.dumps(item, indent=2))
-        
