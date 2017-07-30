bb4135b5cb410bbcc974c65b186498e2ce27c50e
Jacqueline Outka <jacqueline@outka.xyz>
2017-07-29 22:46:54 -0400

    Fix authentication issue

--- get_org_pulls.py
+++ get_org_pulls.py
@@ -2,20 +2,16 @@ import requests
 import json
 import os
 
-def get_token_info():
-    return {'Authorization': 'token %s' % os.environ['GITHUB_TOKEN'] }
-
-def get_repo_names(url, headers):
-    organization_data = requests.get(url, headers).json()
+def get_repo_names(url):
+    organization_data = requests.get(url, auth=(os.environ['GITHUB_TOKEN'], 'x-oauth-basic')).json()
     return [organization_data[index][u'name'] for (index, item) in enumerate(organization_data)]
 
-def get_pull_requests(repo_names, headers):
-    return [requests.get('https://api.github.com/repos/lodash/{}/pulls'.format(repo), headers).json() for repo in repo_names]
+def get_pull_requests(repo_names):
+    return [requests.get('https://api.github.com/repos/lodash/{}/pulls'.format(repo), auth=(os.environ['GITHUB_TOKEN'], 'x-oauth-basic')).json() for repo in repo_names]
 
 if __name__ == '__main__':
-    headers = get_token_info()
-    repo_names = get_repo_names('https://api.github.com/orgs/lodash/repos', headers)
-    pull_requests = get_pull_requests(repo_names, headers)
+    repo_names = get_repo_names('https://api.github.com/orgs/lodash/repos')
+    pull_requests = get_pull_requests(repo_names)
     for item in pull_requests:
         print(json.dumps(item, indent=2))
         
