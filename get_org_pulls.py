037df972c1cade6b15fd87ac0ae0b3569a41ed0d
Jacqueline Outka <jacqueline@outka.xyz>
2017-07-29 21:41:57 -0400

    Update to pull information about authentication token from system

--- get_org_pulls.py
+++ get_org_pulls.py
@@ -1,16 +1,21 @@
 import requests
 import json
+import os
 
-def get_repo_names(url):
-    organization_data = requests.get(url).json()
+def set_up_authentication():
+    return {'Authorization': 'token %s' % os.environ['GITHUB_TOKEN'] }
+
+def get_repo_names(url, headers):
+    organization_data = requests.get(url, headers).json()
     return [organization_data[index][u'name'] for (index, item) in enumerate(organization_data)]
 
-def get_pull_requests(repo_names):
-    return [requests.get('https://api.github.com/repos/lodash/{}/pulls'.format(repo)).json() for repo in repo_names]
+def get_pull_requests(repo_names, headers):
+    return [requests.get('https://api.github.com/repos/lodash/{}/pulls'.format(repo), headers).json() for repo in repo_names]
 
 if __name__ == '__main__':
-    repo_names = get_repo_names('https://api.github.com/orgs/lodash/repos')
-    pull_requests = get_pull_requests(repo_names)
+    headers = set_up_authentication()
+    repo_names = get_repo_names('https://api.github.com/orgs/lodash/repos', headers)
+    pull_requests = get_pull_requests(repo_names, headers)
     for item in pull_requests:
         print(item)
         print("\n")
