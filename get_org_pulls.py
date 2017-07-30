4e9986b383bd4558558fbcc86e023a27c1500092
Jacqueline Outka <jacqueline@outka.xyz>
2017-07-29 22:22:45 -0400

    Cleanup

--- get_org_pulls.py
+++ get_org_pulls.py
@@ -2,7 +2,7 @@ import requests
 import json
 import os
 
-def set_up_authentication():
+def get_token_info():
     return {'Authorization': 'token %s' % os.environ['GITHUB_TOKEN'] }
 
 def get_repo_names(url, headers):
@@ -13,9 +13,9 @@ def get_pull_requests(repo_names, headers):
     return [requests.get('https://api.github.com/repos/lodash/{}/pulls'.format(repo), headers).json() for repo in repo_names]
 
 if __name__ == '__main__':
-    headers = set_up_authentication()
+    headers = get_token_info()
     repo_names = get_repo_names('https://api.github.com/orgs/lodash/repos', headers)
     pull_requests = get_pull_requests(repo_names, headers)
     for item in pull_requests:
-        print(item)
-        print("\n")
+        print(json.dumps(item, indent=2))
+        
